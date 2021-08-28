7e0138e7f38b9e2f1400c3add7d850955faad500
Alex Lewis <alexvlewis@gmail.com>
2021-08-28 13:37:18 -0700

    Scorpio write script

--- scripts/write-scorpio.ts
+++ scripts/write-scorpio.ts
@@ -5,20 +5,34 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
     process.env.JSONRPC_URL || 'http://localhost:8545'
   )
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || '', provider)
+  console.log('Wallet:', wallet.address)
+
+  const gas = process.env.GAS || '50'
+  const nonce = await wallet.getTransactionCount()
+  console.log('Gas:', gas)
+  console.log('Nonce:', nonce)
 
   const token = new ethers.Contract(
     process.env.CONTRACT || '0x5a2BE6CF65A2F019183058Cb8eBe828834F40dE6',
     ScorpioNFT.abi,
-    provider
+    wallet
   )
 
-  console.log('Read from Scorpio contract:', token.address)
+  console.log('Write to Scorpio contract:', token.address)
   console.log('Function:', process.env.FUNCTION)
   console.log('Args:', process.env.ARGS)
 
-  const result = await token[process.env.FUNCTION || ''](...JSON.parse(process.env.ARGS || '[]'))
+  const result = await token[process.env.FUNCTION || ''](
+    ...JSON.parse(process.env.ARGS || '[]'),
+    {
+      gasLimit: 10e6,
+      gasPrice: ethers.utils.parseUnits(gas, 'gwei'),
+    }
+  )
 
-  console.log('Result:', result)
+  await result.wait()
+  console.log('Write tx:', result.hash)
 }
 
 main()
